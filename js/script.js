16c535f684df17bb11bc2d7e55be93fdd9d275df
dongsoo <keass9@gmail.com>
2015-04-22 19:04:03 +0900

    생성자함수로 만들기~ 시작

--- js/script.js
+++ js/script.js
@@ -1,3 +1,37 @@
 "use strict";
 
+/*
+* 스핀박스 만들어보기
+* 1. 인풋+버튼2 다 있다고 가정
+* 2. 0 으로 초기화
+* 3. 마우스 클릭으로 증가 / 감소 */
 
+function Spinbox(val){
+    var inputEl = document.getElementById(val);
+    var upEl = document.getElementById('btn_up');
+    var dnEl = document.getElementById('btn_dn');
+
+    console.log(inputEl);
+
+    this.zeroInit();
+    this.eventup();
+    this.eventdn();
+}
+
+Spinbox.prototype = {
+    zeroInit:function(){
+        console.log(this);
+    },
+    eventup:function(){
+        upEl.addEventListener('click',function(){
+            var nNumber = parseInt(inputEl.value);
+            inputEl.value = nNumber +1;
+        });
+    }s,
+    eventdn:function(){
+        dnEl.addEventListener('click',function(){
+            var nNumber = parseInt(inputEl.value);
+            inputEl.value = nNumber -1;
+        });
+    }
+};
